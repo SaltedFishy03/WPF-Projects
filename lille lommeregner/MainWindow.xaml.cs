ecd14670758a3eb6c0cdb24139e2ce5621bb4ed9
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-09-15 14:58:55 +0200

    fædiggjort første del af den lille lommerenger

--- lille lommeregner/MainWindow.xaml.cs
+++ lille lommeregner/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,40 @@ namespace lille_lommeregner
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void plus_click(object sender, RoutedEventArgs e)
+        {
+            double resultat = RegneFunktion();
+            tb_resultat.Text = resultat.ToString();
+        }
+
+        private double RegneFunktion(char reggneTegn)
+        {
+            double tal1 = 0;
+            double tal2 = 0;
+            double resultat = 0;
+
+
+
+            try
+            {
+                tal1 = Convert.ToDouble(tb_tal1.Text);
+                tal2 = Convert.ToDouble(tb_tal2.Text);
+                resultat = tal1 + tal2;
+            }
+            catch (Exception exc)
+            {
+
+                MessageBox.Show("Indtast kun tal!!!", exc.Message);
+
+
+
+
+
+            }
+
+            return resultat;
         }
     }
 }
