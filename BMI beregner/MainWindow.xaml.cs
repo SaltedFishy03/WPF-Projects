344f368504e4af7f81d63ec293c2e49da585fa81
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-10-06 15:05:00 +0200

    bmi wpf færdig og på begyndt c#

--- BMI beregner/MainWindow.xaml.cs
+++ BMI beregner/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace BMI_beregner
         {
             InitializeComponent();
         }
+        
+        private double BMIberegner(double weight, double height)
+        {
+            ;
+            double bmi = weight / height;
+            return bmi;
+            
+        }
+
+        
+
+        private void btn_Beregn_Click(object sender, RoutedEventArgs e)
+        {
+            string _weight = tb_weight.Text;
+            string _height = tb_height.Text;
+
+            double BMI = BMIberegner(double.Parse(_weight), double.Parse(_height)); 
+            
+          if(cb_gender.Text == "Mand")
+            {
+                if (true)
+                {
+
+                } else if ()
+                {
+
+                }
+                switch(BMI)
+                {
+                    case < 20:
+                        MessageBox.Show($"Du er en Mand på {_height} cm og {_weight} Kg. \n Hvilket betyder at du er undervægtig");
+                        return;
+                    case >= 20 && < 25:
+                        return;
+                }
+            } else
+            {
+
+            }
+        }
     }
 }
