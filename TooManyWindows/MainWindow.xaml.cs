94e551e522a3cd022dc14a3677ef66d20a6a6684
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-10-11 13:03:00 +0200

    færdig med TooManyWindows

---
+++ TooManyWindows/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TooManyWindows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            vindue2 vindue = new vindue2("");
+            vindue.Navn = "Mathias";
+            tb_1.Text = vindue.Navn;
+            vindue.ShowDialog();
+
+            if (vindue.DialogResult == true)
+            {
+                tb_1.Text = "hej med dig " + vindue.Navn;
+            }
+            else
+            {
+                tb_1.Text = "Du hedder stadig Mathias";
+            }
+
+
+        }
+    }
+}
---
+++ TooManyWindows/vindue2.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TooManyWindows
+{
+    /// <summary>
+    /// Interaction logic for vindue2.xaml
+    /// </summary>
+    public partial class vindue2 : Window
+    {
+        public string Navn
+        {
+            get
+            {
+                return tb_TypeName.Text;
+            }
+            set
+            {
+                tb_TypeName.Text = value;   
+            }
+        
+        }
+
+        //Overload constructor
+        public vindue2(string navn)
+        {
+            InitializeComponent();
+            //tb_TypeName.Text = navn;
+        }
+
+        
+        private void btn_omdøb_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void btn_fortryd_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
