6c92af55874cf984bdb375a698978528bfda776b
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-10-12 11:40:41 +0200

    xaml done om new project ObjectTransfer

---
+++ ObjectTransfer/Window2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ObjectTransfer
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_ret_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_fortryd_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
