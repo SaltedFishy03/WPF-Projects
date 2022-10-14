1b02b0a652bd9b456c3ef17bc98e9e2135aad157
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-10-14 12:00:09 +0200

    alomst finished the BingoGame project

--- BingoGame/MainWindow.xaml.cs
+++ BingoGame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace BingoGame
 {
@@ -20,9 +22,29 @@ namespace BingoGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<int> InPlayNumbers = new();
+        ObservableCollection<int> UsedNumbers = new();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            lb_unUsedNumbers.ItemsSource = InPlayNumbers;
+            lb_usedNumbers.ItemsSource = UsedNumbers;
+
+            for (int i = 1; i < 91; i++)
+            {
+                InPlayNumbers.Add(i);
+                
+            }
+        }
+        private void btn_pullNumber_Click(object sender, RoutedEventArgs e)
+        {
+            var random = new Random();
+            int pull = random.Next(1, 91);
+            l_currentNumber.Content = pull;
+            UsedNumbers.Add(pull);
+            InPlayNumbers.RemoveAt(pull);
         }
     }
 }
