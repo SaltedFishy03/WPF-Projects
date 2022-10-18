382b7a2ca2c59b7262987d8bbb802a6ca7cb929e
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-10-18 14:59:29 +0200

    new project TwoWayTest startet ( xaml should be done)

---
+++ TwoWayTest/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TwoWayTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person(0, "send", "bendt", 100);
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void btn_data_Click(object sender, RoutedEventArgs e)
+        {
+            string PersonData = person.Fornavn +
+                " " + 
+                person.Efternavn +
+                " har en formue på " +
+                person.Formue +
+                " Kr ";
+
+            MessageBox.Show(PersonData);
+        }
+    }
+}
---
+++ TwoWayTest/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoWayTest
+{
+    public class Person
+    {
+        public int ID;
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+        public int Formue { get; set; }
+
+        public Person(int ID, string Fornavn, string Efternavn, int Formue)
+        {
+            this.ID = ID;
+            this.Fornavn = Fornavn;
+            this.Efternavn = Efternavn;
+            this.Formue = Formue;
+
+            
+            
+        }
+    }
+}
