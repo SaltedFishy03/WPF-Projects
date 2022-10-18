a24a4f14ffbc1eea94354efbbac934b259cfbff4
Mathias Schaltz Hansen <math740v@elev.techcollege.dk>
2022-10-18 10:08:54 +0200

    BingoGame random multiple pull fixed + BingoGame Finish

--- BingoGame/MainWindow.xaml.cs
+++ BingoGame/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -35,16 +36,31 @@ namespace BingoGame
             for (int i = 1; i < 91; i++)
             {
                 InPlayNumbers.Add(i);
-                
+
             }
         }
         private void btn_pullNumber_Click(object sender, RoutedEventArgs e)
         {
-            var random = new Random();
-            int pull = random.Next(1, 91);
-            l_currentNumber.Content = pull;
-            UsedNumbers.Add(pull);
-            InPlayNumbers.RemoveAt(pull);
+            if (InPlayNumbers.Count > 0)
+            {
+                var random = new Random();
+                int pull;
+
+                do
+                {
+                    pull = random.Next(1, 91);
+
+                } while (!InPlayNumbers.Contains(pull));
+
+                l_currentNumber.Content = pull;
+                UsedNumbers.Add(pull);
+                InPlayNumbers.Remove(pull);
+            }
+            else
+            {
+                MessageBox.Show("ikke flere tal!");
+            }
+
         }
     }
 }
